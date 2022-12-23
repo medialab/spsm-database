a72c2610c56f0f06df5554f32be54afdecb5de0c
Kelly Christensen <kelly.christensen@sciencespo.fr>
2022-12-23 06:26:47 +0100

    fix subdirectories in archive

--- archive/archive.sh
+++ archive/archive.sh
@@ -16,23 +16,22 @@ xsv select archive_url,url_id $DATAFILE |
     log_dir=log_$first_char
     path_dir=path_$first_char
     mkdir -p $log_dir $path_dir 
-    logfile="${url_id}_log"  # assign variable for log file
-    #pathsfile="${url_id}_paths"  # assign variable for path file
-    # timestamp=$(date +"%F %H:%M:%S") # assign variable for timestamp --> not usefull because already the first line of the logfile
 
     curl -sL "https://web.archive.org/save/$archive_url" > /dev/null
 
     cd $ARCHIVEDIR  # go into the archive
+
+      logfile="../../${log_dir}/${url_id}_log"
+      pathsfile="../../${path_dir}/${url_id}_paths"
+
       # do everything you need to do
-      wget -E -H -k -K -p "$archive_url" -o ../../$log_dir/${url_id}_log
-      main_pathfile=$(cat ../../$log_dir/${url_id}_log | grep "Sauvegarde" | head -1)  
+      wget -E -H -k -K -p "$archive_url" -o $logfile
+      main_pathfile=$(cat $logfile | grep "Sauvegarde" | head -1)  
       echo "url: ${archive_url}" # url of the fake news 
       echo "logfile: ${logfile}" # hash 
       echo "path_html: ${main_pathfile}" # path of the saved html
-      # echo "timestamp:${timestamp}" # timestamp of the wget command but alreadw in the logfile
-      # echo "URL archived at: ${timestamp}" >> logfile #the timestamp is already in the logfile
-      cat ../../$log_dir/${url_id}_log | grep "Sauvegarde" > ../../$path_dir/${url_id}_paths
-      echo "path_file: ${url_id}_paths" 
+      cat $logfile | grep "Sauvegarde" > $pathsfile
+      echo "path_file: $pathsfile" 
       echo ""
 
     cd .. # go back to the top-level, where the script is saved / from where the script is deployed
