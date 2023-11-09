6ec34732725ef42f858dd23079005ed8feb7cb68
Kelly Christensen <kelly.christensen@sciencespo.fr>
2023-11-09 08:40:58 +0100

    try new export

--- src/enrich_database/sql/timestudy.sql
+++ src/enrich_database/sql/timestudy.sql
@@ -8,6 +8,7 @@ select foo.fact_check_time,
        foo.search_by_title,
        foo.tweet_id,
        foo.retweeted_id,
+       foo.quoted_id,
        foo.user_id,
        foo.unique_condor_id,
        foo.unique_defacto_id,
@@ -19,7 +20,10 @@ select foo.fact_check_time,
                                                                                                                   from foo.diff) as diff_seconds
 from
   (select t.timestamp_utc - c.fact_check_time as diff,
-          c.fact_check_time,
+          case
+              when c.defacto_id is not null then c.fact_check_time + interval '1 hour'
+              else c.fact_check_time
+          end,
           c.fact_checked_true,
           c.fact_checked_false,
           c.id as claim_id,
@@ -29,6 +33,7 @@ from
           tq.tweet_id,
           t.timestamp_utc as tweet_time,
           t.retweeted_id,
+          t.quoted_id,
           t.user_id,
           c.condor_table_id as unique_condor_id,
           c.defacto_id as unique_defacto_id,
