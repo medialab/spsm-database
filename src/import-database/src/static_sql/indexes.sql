654966528759c3aa0781e95389e073e2335bddeb
Kelly Christensen <kelly.christensen@sciencespo.fr>
2023-11-10 10:38:00 +0100

    indexes

---
+++ src/import-database/src/static_sql/indexes.sql
@@ -0,0 +1,25 @@
+drop index if exists tweet_user_id_index ;
+
+
+create index tweet_user_id_index on tweet using hash (user_id) ;
+
+
+drop index if exists tweet_lang_index ;
+
+
+create index tweet_lang_index on tweet using hash (lang) ;
+
+
+drop index if exists tweet_retweeted_id_index ;
+
+
+create index tweet_retweeted_id_index on tweet using hash (retweeted_id) ;
+
+
+drop index if exists tweet_quoted_id_index ;
+
+
+create index tweet_quoted_id_index on tweet using hash (quoted_id) ;
+
+
+drop index if exists tweet_claim_search_by_title_index on tweet_claim using hash (search_by_title) ;
