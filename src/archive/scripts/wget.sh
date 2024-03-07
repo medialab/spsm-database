1ae7daf3d9ab9c8a8cb7be546054b1ec90f7b958
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2024-03-07 12:08:06 +0100

    add a recent chrome user agent to wget script

--- src/archive/scripts/wget.sh
+++ src/archive/scripts/wget.sh
@@ -6,6 +6,6 @@ LOG_FILE=../../$3
 PATHS_FILE=../../$4
 
 cd $ARCHIVE_DIR
-wget -E -H -k -K -p --ignore-tags=source,audio --timeout=120 --tries=10 "$ARCHIVE_URL" -o $LOG_FILE
+wget -E -H -k -K -p --ignore-tags=source,audio --timeout=120 --tries=10 --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3" "$ARCHIVE_URL" -o $LOG_FILE
 cat $LOG_FILE | grep -E "Sauvegarde|Saving" > $PATHS_FILE
 cd -
